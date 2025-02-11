243e18fd4d9b5e38fa958a273d21bb445eddb21e
felania-f <felania71@gmail.com>
2025-02-11 11:45:49 +0100

    #Models/Panier_utilisateur

--- src/app/Models/Panier.ts
+++ src/app/Models/Panier.ts
@@ -1,4 +1,5 @@
 import { Articles } from "./Articles";
+import { Utilisateurs } from "./Utilisateurs";
 
 export class Panier {
 
@@ -7,13 +8,16 @@ export class Panier {
   private prix: number;
   private prix_Total: number;
   private ls_articles: Articles[];
+  private utilisateur: Utilisateurs[];
 
-  constructor(article_Id: number, quantité: number, prix: number, prix_Total: number, ls_articles: Articles[]) {
+
+  constructor(article_Id: number, quantité: number, prix: number, prix_Total: number, ls_articles: Articles[], utilisateur: Utilisateurs[]) {
     this.article_Id = article_Id;
     this.quantité = quantité;
     this.prix = prix;
     this.prix_Total = prix_Total;
     this.ls_articles = ls_articles;
+    this.utilisateur = utilisateur;
   }
 
   getArticle_Id(): number {
@@ -45,6 +49,9 @@ export class Panier {
   }
   setLs_Articles(ls_articles: Articles[]) {
     this.ls_articles = ls_articles;
+  }
+  getUtilisateur(): Utilisateurs[] {
+    return this.utilisateur;
   }
 
 }
